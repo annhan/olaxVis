3d0405a7315514bf67fc1fa48ac000b3943e3bad
mwork <annhandt09@gmail.com>
2021-11-28 13:31:53 +0700

    .

---
+++ OlaxU60/OlaxU60/OlaxU60/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace OlaxU60
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.resetOlax = new System.Windows.Forms.Button();
+            this.getIp = new System.Windows.Forms.Button();
+            this.resultCheckIP = new System.Windows.Forms.TextBox();
+            this.dataGet = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(30, 26);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1065, 534);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGet);
+            this.tabPage1.Controls.Add(this.resultCheckIP);
+            this.tabPage1.Controls.Add(this.getIp);
+            this.tabPage1.Controls.Add(this.resetOlax);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1057, 505);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "OLAX U80";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 71);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // resetOlax
+            // 
+            this.resetOlax.Location = new System.Drawing.Point(86, 64);
+            this.resetOlax.Name = "resetOlax";
+            this.resetOlax.Size = new System.Drawing.Size(170, 80);
+            this.resetOlax.TabIndex = 0;
+            this.resetOlax.Text = "resetOlax";
+            this.resetOlax.UseVisualStyleBackColor = true;
+            this.resetOlax.UseWaitCursor = true;
+            this.resetOlax.Click += new System.EventHandler(this.resetOlax_Click);
+            // 
+            // getIp
+            // 
+            this.getIp.Location = new System.Drawing.Point(86, 167);
+            this.getIp.Name = "getIp";
+            this.getIp.Size = new System.Drawing.Size(170, 80);
+            this.getIp.TabIndex = 1;
+            this.getIp.Text = "getIp";
+            this.getIp.UseVisualStyleBackColor = true;
+            this.getIp.UseWaitCursor = true;
+            this.getIp.Click += new System.EventHandler(this.getIp_Click);
+            // 
+            // resultCheckIP
+            // 
+            this.resultCheckIP.Location = new System.Drawing.Point(86, 269);
+            this.resultCheckIP.Name = "resultCheckIP";
+            this.resultCheckIP.Size = new System.Drawing.Size(170, 22);
+            this.resultCheckIP.TabIndex = 2;
+            // 
+            // dataGet
+            // 
+            this.dataGet.Location = new System.Drawing.Point(526, 63);
+            this.dataGet.Multiline = true;
+            this.dataGet.Name = "dataGet";
+            this.dataGet.Size = new System.Drawing.Size(270, 325);
+            this.dataGet.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 591);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button getIp;
+        private System.Windows.Forms.Button resetOlax;
+        private System.Windows.Forms.TextBox dataGet;
+        private System.Windows.Forms.TextBox resultCheckIP;
+    }
+}
+
---
+++ OlaxU60/OlaxU60/OlaxU60/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace OlaxU60
+{
+    public partial class Form1 : Form
+    {
+        string[] listIP = { "", "", "", "", "", "", "", "", "", "" };
+        int numberIP = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void getmyIp()
+        {
+            int kq = 0;
+            var url = "https://api.myip.com";
+
+            var request = WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            var reader = new StreamReader(webStream);
+            var data = reader.ReadToEnd();
+            dynamic json = JsonConvert.DeserializeObject(data);
+            //dataGet.ResetText();
+            listIP[numberIP] = json["ip"].ToString();
+            dataGet.ResetText();
+            dataGet.AppendText("IP Hien Tai: ");
+            dataGet.AppendText(json["ip"].ToString());
+            dataGet.AppendText("\r\n");
+            dataGet.AppendText("\r\n");
+            for (int i = 0; i < 10; i++)
+            {
+                dataGet.AppendText(i.ToString());
+                dataGet.AppendText(" : ");
+                if (i == numberIP)
+                {
+                    dataGet.AppendText("vi tri IP hien Tai");
+                }
+                else
+                {
+                    dataGet.AppendText(listIP[i]);
+                    if (String.Equals(listIP[i], listIP[numberIP]))
+                    {
+                        kq = 1;
+                    }
+
+                }
+                dataGet.AppendText("\r\n");
+            }
+            numberIP++;
+            if (numberIP > 9)
+            {
+                numberIP = 0;
+            }
+            resultCheckIP.ResetText();
+            if (kq == 0)
+                resultCheckIP.Text = "OK";
+            else
+                resultCheckIP.Text = "Trung IP";
+        }
+        private void resetOlaxData()
+        {
+            var values = new Dictionary<string, string>
+            {
+                { "goformId", "REBOOT_DEVICE" }
+            };
+
+            var data = new FormUrlEncodedContent(values);
+
+            var url = "http://192.168.0.1/goform/goform_set_cmd_process";
+            var client = new HttpClient();
+
+            var response = client.PostAsync(url, data);
+
+            //string result = response.Content.ReadAsStringAsync().Result;
+            //Console.WriteLine(result);
+        }
+        private void resetOlax_Click(object sender, EventArgs e)
+        {
+            resetOlaxData();
+        }
+
+        private void getIp_Click(object sender, EventArgs e)
+        {
+            getmyIp();
+        }
+    }
+}
