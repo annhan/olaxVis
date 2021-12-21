d693e3ba183b8515e0610a3434ab95bef93c4b72
mwork <annhandt09@gmail.com>
2021-12-21 22:42:04 +0700

    update for E3372

--- OlaxU60/OlaxU60/OlaxU60/Form1.Designer.cs
+++ OlaxU60/OlaxU60/OlaxU60/Form1.Designer.cs
@@ -29,45 +29,86 @@
         private void InitializeComponent()
         {
             this.Other = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.resetOlax = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.namecard = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.dataGet = new System.Windows.Forms.TextBox();
             this.resultCheckIP = new System.Windows.Forms.TextBox();
             this.getIp = new System.Windows.Forms.Button();
-            this.resetOlax = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button2 = new System.Windows.Forms.Button();
-            this.namecard = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.ChangeNetwork = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.Other.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
             // Other
             // 
+            this.Other.Controls.Add(this.tabPage3);
             this.Other.Controls.Add(this.tabPage1);
             this.Other.Controls.Add(this.tabPage2);
+            this.Other.Controls.Add(this.tabPage4);
             this.Other.Location = new System.Drawing.Point(30, 12);
             this.Other.Name = "Other";
             this.Other.SelectedIndex = 0;
-            this.Other.Size = new System.Drawing.Size(508, 422);
+            this.Other.Size = new System.Drawing.Size(543, 422);
             this.Other.TabIndex = 0;
             // 
-            // tabPage1
+            // resetOlax
             // 
-            this.tabPage1.Controls.Add(this.resetOlax);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(500, 393);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "OLAX U80";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            this.resetOlax.Location = new System.Drawing.Point(14, 25);
+            this.resetOlax.Name = "resetOlax";
+            this.resetOlax.Size = new System.Drawing.Size(170, 80);
+            this.resetOlax.TabIndex = 0;
+            this.resetOlax.Text = "resetOlax";
+            this.resetOlax.UseVisualStyleBackColor = true;
+            this.resetOlax.Click += new System.EventHandler(this.resetOlax_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-27, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tên Card mạng";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // namecard
+            // 
+            this.namecard.Location = new System.Drawing.Point(85, 37);
+            this.namecard.Name = "namecard";
+            this.namecard.Size = new System.Drawing.Size(227, 22);
+            this.namecard.TabIndex = 1;
+            this.namecard.Text = "Ethernet 3";
+            this.namecard.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 60);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Restart network";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // dataGet
             // 
-            this.dataGet.Location = new System.Drawing.Point(544, 109);
+            this.dataGet.Location = new System.Drawing.Point(579, 109);
             this.dataGet.Multiline = true;
             this.dataGet.Name = "dataGet";
             this.dataGet.Size = new System.Drawing.Size(270, 325);
@@ -77,7 +118,7 @@
             // 
             this.resultCheckIP.BackColor = System.Drawing.Color.PeachPuff;
             this.resultCheckIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.resultCheckIP.Location = new System.Drawing.Point(734, 12);
+            this.resultCheckIP.Location = new System.Drawing.Point(769, 12);
             this.resultCheckIP.Multiline = true;
             this.resultCheckIP.Name = "resultCheckIP";
             this.resultCheckIP.Size = new System.Drawing.Size(80, 80);
@@ -87,7 +128,7 @@
             // 
             // getIp
             // 
-            this.getIp.Location = new System.Drawing.Point(544, 12);
+            this.getIp.Location = new System.Drawing.Point(579, 12);
             this.getIp.Name = "getIp";
             this.getIp.Size = new System.Drawing.Size(170, 80);
             this.getIp.TabIndex = 1;
@@ -95,64 +136,134 @@
             this.getIp.UseVisualStyleBackColor = true;
             this.getIp.Click += new System.EventHandler(this.getIp_Click);
             // 
-            // resetOlax
+            // tabPage3
             // 
-            this.resetOlax.Location = new System.Drawing.Point(22, 25);
-            this.resetOlax.Name = "resetOlax";
-            this.resetOlax.Size = new System.Drawing.Size(170, 80);
-            this.resetOlax.TabIndex = 0;
-            this.resetOlax.Text = "resetOlax";
-            this.resetOlax.UseVisualStyleBackColor = true;
-            this.resetOlax.Click += new System.EventHandler(this.resetOlax_Click);
+            this.tabPage3.Controls.Add(this.label6);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.ChangeNetwork);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(535, 393);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Airtel E3372";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.namecard);
+            this.groupBox1.Location = new System.Drawing.Point(48, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(320, 167);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.resetOlax);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(500, 393);
+            this.tabPage1.TabIndex = 3;
+            this.tabPage1.Text = "OLAX U80";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click_1);
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.label1);
-            this.tabPage2.Controls.Add(this.namecard);
-            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.groupBox1);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(500, 393);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Other";
+            this.tabPage2.TabIndex = 4;
+            this.tabPage2.Text = "Reset network";
             this.tabPage2.UseVisualStyleBackColor = true;
-            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
             // 
-            // button2
+            // tabPage4
             // 
-            this.button2.Location = new System.Drawing.Point(167, 53);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(134, 60);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Restart network";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.tabPage4.Controls.Add(this.label2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(500, 393);
+            this.tabPage4.TabIndex = 5;
+            this.tabPage4.Text = "About";
+            this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // namecard
+            // ChangeNetwork
             // 
-            this.namecard.Location = new System.Drawing.Point(167, 14);
-            this.namecard.Name = "namecard";
-            this.namecard.Size = new System.Drawing.Size(227, 22);
-            this.namecard.TabIndex = 1;
-            this.namecard.Text = "Ethernet 3";
-            this.namecard.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.ChangeNetwork.Location = new System.Drawing.Point(15, 43);
+            this.ChangeNetwork.Name = "ChangeNetwork";
+            this.ChangeNetwork.Size = new System.Drawing.Size(170, 80);
+            this.ChangeNetwork.TabIndex = 1;
+            this.ChangeNetwork.Text = "ChangeNetwork";
+            this.ChangeNetwork.UseVisualStyleBackColor = true;
+            this.ChangeNetwork.Click += new System.EventHandler(this.ChangeNetwork_Click);
             // 
-            // label1
+            // label2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Tên Card mạng";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "@annhandt09";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AllowDrop = true;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(479, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Với Sim Mobile cần vào profile và tạo 1 profile manual ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Profile: Test  APN: internet";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AllowDrop = true;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(251, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "để thực hiện việc change IP";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(11, 274);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(245, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "user: mms password: mms";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(861, 449);
+            this.ClientSize = new System.Drawing.Size(861, 462);
             this.Controls.Add(this.Other);
             this.Controls.Add(this.resultCheckIP);
             this.Controls.Add(this.dataGet);
@@ -161,9 +272,14 @@
             this.Text = "Change IP";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Other.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
-            this.tabPage2.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,8 +288,6 @@
         #endregion
 
         private System.Windows.Forms.TabControl Other;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button getIp;
         private System.Windows.Forms.Button resetOlax;
         private System.Windows.Forms.TextBox dataGet;
@@ -181,6 +295,17 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox namecard;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button ChangeNetwork;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- OlaxU60/OlaxU60/OlaxU60/Form1.cs
+++ OlaxU60/OlaxU60/OlaxU60/Form1.cs
@@ -7,18 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.IO;
 using Newtonsoft.Json;
-
+using System.Windows;
 namespace OlaxU60
 {
     public partial class Form1 : Form
     {
         string[] listIP = { "", "", "", "", "", "", "", "", "", "" };
         int numberIP = 0;
+        string typeapn = "";
+        int dem = 0;
         public Form1()
         {
             InitializeComponent();
@@ -28,8 +30,12 @@ namespace OlaxU60
         {
 
         }
+        private static void Initialize()
+        {
+        }
         private void getmyIp()
         {
+            Initialize();
             int kq = 0;
             var url = "https://api.myip.com";
 
@@ -46,8 +52,10 @@ namespace OlaxU60
             }
             catch (Exception e)
             {
+
                 resultCheckIP.Text = "Khong Connect";
                 resultCheckIP.BackColor = Color.Red;
+                MessageBox.Show("Done - Check IP Again");
                 return;
             }
             
@@ -84,17 +92,21 @@ namespace OlaxU60
                 numberIP = 0;
             }
             resultCheckIP.ResetText();
+            string text = "";
             if (kq == 0)
             {
                 resultCheckIP.BackColor = Color.LightGreen;
+                text= "Dont -OK";
                 resultCheckIP.Text = "OK";
             }
             else
             {
+                text = "Dont - Trung IP";
                 resultCheckIP.Text = "Trung IP";
                 resultCheckIP.BackColor = Color.Red;
             }
-                
+            MessageBox.Show(text);
+
         }
         private void resetOlaxData()
         {
@@ -183,6 +195,151 @@ namespace OlaxU60
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        public async Task<string> checkmode()
+        {
+            string URL = "http://192.168.8.1/reqproc/proc_get?cmd=apn_mode";
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(URL);
+            string responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+            /*
+                HttpResponseMessage response1 = await client.GetAsync("http://192.168.8.1/reqproc/proc_get?cmd=apn_mode");
+                if (response1.IsSuccessStatusCode)
+                {
+                    //var result = response1.Content.ReadAsStringAsync().Result;
+                   // dynamic json = JsonConvert.DeserializeObject(result);
+                    typeapn = "fdfd";
+                    // typeapn = json["apn_mode"].ToString();
+                    // var s = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
+                }
+                else
+                {
+                    typeapn = "fail";
+                }*/
+            
+            /*
+            var client = new HttpClient();
+            Debug.WriteLine("result:: ");
+            var response1 = await client.GetAsync("http://192.168.8.1/reqproc/proc_get?cmd=apn_mode");
+            //typeapn = response1;
+            
+            if (response1.IsSuccessStatusCode)
+            {
+                var result = response1.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(result);
+                typeapn = "fdfd";
+               // typeapn = json["apn_mode"].ToString();
+                // var s = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
+            }
+            else
+            {
+                typeapn = "fail";
+            }*/
+        }
+        private async void ChangeNetwork_Click(object sender, EventArgs e)
+        {
+
+            //http://192.168.8.1/reqproc/proc_get?cmd=apn_mode
+            var disconect = new Dictionary<string, string>
+            {
+                { "isTest", "false" },
+                { "notCallback", "true" },
+                { "goformId", "DISCONNECT_NETWORK" },
+            };
+            var connect = new Dictionary<string, string>
+            {
+                { "isTest", "false" },
+                { "notCallback", "true" },
+                { "goformId", "CONNECT_NETWORK" },
+            };
+//isTest=false&goformId=APN_PROC_EX&apn_mode=auto
+            var apn1 = new Dictionary<string, string>
+            {
+                { "isTest", "false" },
+                { "goformId", "APN_PROC_EX" },
+                { "apn_mode", "auto" },
+            };
+//isTest=false&goformId=APN_PROC_EX&apn_mode=manual&apn_action=set_default&set_default_flag=1&pdp_type=IP&index=1
+
+            var apn2 = new Dictionary<string, string>
+            {
+                { "isTest", "false" },
+                { "goformId", "APN_PROC_EX" },
+                { "apn_mode", "manual" },
+                { "apn_action", "set_default" },
+                { "set_default_flag", "1" },
+                { "pdp_type", "IP" },
+                 { "index", "1" }
+            };
+            //textBox1.Text = "Disconnect";
+            //textBox1.BackColor = Color.Red;
+            var client = new HttpClient();
+            dem++;
+            var data = new FormUrlEncodedContent(disconect);
+            var url = "http://192.168.8.1/reqproc/proc_post";
+            var response = client.PostAsync(url, data);
+            //textBox1.Text = "Change";
+            //textBox1.BackColor = Color.Red;
+            if (dem % 2 == 0)
+            {
+                data = new FormUrlEncodedContent(apn1);
+            }
+            else
+            {
+                data = new FormUrlEncodedContent(apn2);
+            }
+            response = client.PostAsync(url, data);
+            System.Threading.Thread.Sleep(200);
+            //textBox1.Text = "Connect";
+            //textBox1.BackColor = Color.Red;
+            data = new FormUrlEncodedContent(connect);
+            response = client.PostAsync(url, data);
+            System.Threading.Thread.Sleep(2200);
+            getmyIp();
+            //textBox1.Text = typeapn;
+            //textBox1.BackColor = Color.LightGreen;
+
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
         {
 
         }
